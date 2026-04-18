dd10699d3ef841e760b17fbded92433bb64aec12
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-18 22:32:26 +0530

    Time: 0 ms (100%), Space: 16.9 MB (34.75%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
@@ -5,6 +5,7 @@ public:
         for(string word:words){
             ans+=word;
             if(ans.length()==s.length() && s==ans)  return true;
+            if(s.find(ans)==-1) return false;
         }
         return false;
     }
