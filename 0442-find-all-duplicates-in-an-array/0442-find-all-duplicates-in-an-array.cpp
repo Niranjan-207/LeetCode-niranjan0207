a6033749b39446593f70d3689f65992ad5885674
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-23 23:39:58 +0530

    Time: 3 ms (73.84%), Space: 48.4 MB (81.74%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,48 +1,19 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        // int n=nums.size();
-        // for(int i=0;i<n;i++){
-        //     int num=nums[i]-1;
-        //     if(num<1)   continue;
-        //     int idx=i;
-        //     while(num != idx){
-        //         int nnum=nums[idx]-1;
-        //         if(nums[num]>0){
-        //             nums[num]=-1;
-        //         }else{
-        //             nums[num]-=1;
-        //         }
-        //         //nums[num]-=1;
-        //         num=nnum;
-        //         idx=num;
-        //     }
-        // }
-        
-        // for(int num:nums)   cout<<num<<" ";
-        // vector<int> ans;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==-2)  ans.push_back(i+1);
-        // }
-        // return ans;
-        int i = 0, n = nums.size();
-
-        while (i < n) {
-            int correct = nums[i] - 1;
-
-            if (nums[i] != nums[correct])
-                swap(nums[i], nums[correct]);
-            else
+        int i=0,n=nums.size();
+        while(i<n){
+            int actual=nums[i]-1;
+            if(nums[actual]!=nums[i]){
+                swap(nums[actual],nums[i]);
+            }else{
                 i++;
+            }
         }
-
         vector<int> ans;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1)
-                ans.push_back(nums[i]);
+        for(int i=0;i<n;i++){
+            if(i+1!=nums[i])    ans.push_back(nums[i]);
         }
-
         return ans;
     }
 };
