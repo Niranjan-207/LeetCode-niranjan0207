a8f580f3dc70ded40581f3b9f23e87af04da22f3
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-29 13:30:21 +0530

    Time: 0 ms (100%), Space: 8.2 MB (15.32%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[11][2][11];
+    int solve(string s,int idx,bool tight,int cnt){
+        if(idx==s.length()) return cnt;
+
+        if(dp[idx][tight][cnt]!=-1) return dp[idx][tight][cnt];
+        int lb=0;
+        int ub=(tight)?s[idx]-'0':9;
+        int ans=0;
+        for(int dig=lb;dig<=ub;dig++){
+            ans+=solve(s,idx+1,(tight && dig==ub),(dig==1)?cnt+1:cnt);
+        }
+        return dp[idx][tight][cnt]=ans;
+    }
+
+    int countDigitOne(int n) {
+        string s=to_string(n);
+        memset(dp,-1,sizeof(dp));
+        return solve(s,0,1,0);//idx,tight,cnt
+    }
+};
