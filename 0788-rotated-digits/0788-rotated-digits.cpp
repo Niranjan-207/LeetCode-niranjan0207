c30b34c206f032e6182752d9992d5a4c4d39c1db
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-02 12:25:12 +0530

    Time: 74 ms (5.09%), Space: 9 MB (28.95%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+// set<char> se={'0','1','8','2','5','6','9'};
+
+    int rotatedDigits(int n) {
+        set<char> se={'3','4','7'};
+        set<char> sr={'0','1','8'};
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            string s=to_string(i);
+            bool flag=true;
+            bool can=false;
+            for(char c:s){
+                if(sr.find(c)==sr.end())   can=true;
+                if(se.find(c)!=se.end()){
+                    flag=false;
+                    break;
+                }
+            }
+            
+            if(flag && can){    cout<<i<<" ";cnt++;}
+        }
+        return cnt;
+    }
+
+};
