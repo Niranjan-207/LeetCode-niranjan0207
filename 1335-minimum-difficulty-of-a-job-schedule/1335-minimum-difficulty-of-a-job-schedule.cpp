050aa6768e17352df630faa972fd3fa824777e31
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-05 22:09:49 +0530

    Time: 63 ms (14.02%), Space: 11 MB (62.94%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    unordered_map<string,int> dp;
-    string encode(int idx,int ma,int cd){
-        return (to_string(idx)+'#'+to_string(ma)+'#'+to_string(cd));
-    }
-    int f(int idx,int ma,vector<int>& arr,int cd,int d){
-        if(cd>d)    return 1e9;
-        if(idx==arr.size() && cd==d)    return ma;
-        if(idx==arr.size()) return 1e9;
-
-        string key=encode(idx,ma,cd);
-        if(dp.count(key)) return dp[key];
-        int newDay=ma+f(idx+1,arr[idx],arr,cd+1,d);
-        int sameDay=f(idx+1,max(ma,arr[idx]),arr,cd,d);
-        return dp[key]=min(newDay,sameDay);
+   
+   
+    int f(int idx,int& n,vector<int>& arr,int d,vector<vector<int>>& dp){
+       if(d==1){
+            return *max_element(begin(arr)+idx,end(arr));
+       }
+        if(dp[idx][d]!=-1)  return dp[idx][d];
+       int finalValue=INT_MAX;
+       int curMax=INT_MIN;
+       for(int i=idx;i<=n-d;i++){
+        curMax=max(curMax,arr[i]);
+        int segAns=curMax+f(i+1,n,arr,d-1,dp);
+        finalValue=min(finalValue,segAns);
+       }
+       return dp[idx][d]=finalValue;
     }
 
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n=jobDifficulty.size();
         if(n<d) return -1;
         if(n==d)    return accumulate(begin(jobDifficulty),end(jobDifficulty),0);
-        
-        return f(1,jobDifficulty[0],jobDifficulty,1,d);   //idx,max,arr,cd,d    
+        vector<vector<int>> dp(n,vector<int>(d+1,-1));
+        return f(0,n,jobDifficulty,d,dp);   //idx,arr,cd,d    
     }
 };
