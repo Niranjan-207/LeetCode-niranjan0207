1314670a233b616edb83d5ac132b903af8c13d03
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-07 12:04:42 +0530

    Time: 6 ms (68.78%), Space: 34.9 MB (89.08%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -3,34 +3,31 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int n=maze.size();
         int m=maze[0].size();
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        
         vector<int> X={1,0,-1,0};
         vector<int> Y={0,1,0,-1};
 
         int cnt=0;
         queue<pair<int,int>> q;
         int ex=entrance[0],ey=entrance[1];
-        vis[ex][ey]=true;
+        maze[ex][ey]='-';
         q.push({ex,ey});
 
         while(!q.empty()){
             int qn=q.size();
             cnt++;
             for(int i=0;i<qn;i++){
-                cout<<cnt<<" ";
                 auto [x,y]=q.front();
                 q.pop();
-
-                
 
                 for(int i=0;i<4;i++){
                     int nx=x+X[i];
                     int ny=y+Y[i];
 
-                    if(nx<n && nx>=0 && ny<m && ny>=0 && !vis[nx][ny] && maze[nx][ny]=='.'){
+                    if(nx<n && nx>=0 && ny<m && ny>=0 && maze[nx][ny]!='-' && maze[nx][ny]=='.'){
                         if((nx==0 || nx==n-1 || ny==0 || ny==m-1) && (nx!=ex || ny!=ey) )    return cnt;
                         q.push({nx,ny});
-                        vis[nx][ny]=true;
+                        maze[nx][ny]='-';
                     }
                 }
             }
