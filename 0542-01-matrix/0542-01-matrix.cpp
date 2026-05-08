30883712f00ab18b99bcbe5114946e84ff405f92
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-08 09:54:14 +0530

    Time: 8 ms (89.85%), Space: 31.8 MB (98.47%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1){
+                    mat[i][j]=1e9;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1e9){
+                    if(i>0)   mat[i][j]=min(mat[i][j],mat[i-1][j]+1);
+                    if(j>0)   mat[i][j]=min(mat[i][j],mat[i][j-1]+1);
+                    //if(i-1>0 && j-1>0)  mat[i][j]=min(mat[i][j],mat[i-1][j-1]+1);
+                }
+            }
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(mat[i][j]!=0){
+                    if(i<n-1)   mat[i][j]=min(mat[i][j],mat[i+1][j]+1);
+                    if(j<m-1)   mat[i][j]=min(mat[i][j],mat[i][j+1]+1);
+                    //if(i<n-1 && j<m-1)  mat[i][j]=min(mat[i][j],mat[i+1][j+1]+1);
+                }
+            }
+        }
+        return mat;
+    }
+};
