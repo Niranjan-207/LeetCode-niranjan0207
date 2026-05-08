d2f5f8acdcd477fe3b21f3c67079740c01073e0a
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-08 09:54:41 +0530

    Time: 3 ms (98.37%), Space: 31.7 MB (98.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -14,8 +14,8 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==1e9){
-                    if(i>0)   mat[i][j]=min(mat[i][j],mat[i-1][j]+1);
-                    if(j>0)   mat[i][j]=min(mat[i][j],mat[i][j-1]+1);
+                    if(i-1>=0)   mat[i][j]=min(mat[i][j],mat[i-1][j]+1);
+                    if(j-1>=0)   mat[i][j]=min(mat[i][j],mat[i][j-1]+1);
                     //if(i-1>0 && j-1>0)  mat[i][j]=min(mat[i][j],mat[i-1][j-1]+1);
                 }
             }
