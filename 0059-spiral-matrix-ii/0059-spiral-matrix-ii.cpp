9c86d25dc6c55bdd4b89ee27e6c112880fd40a72
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-12 09:52:36 +0530

    Time: 0 ms (100%), Space: 9.5 MB (40.7%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+        //vector<vector<int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+        vector<int> dir={0,1,0,-1,0};
         vector<vector<int>> ans(n,vector<int>(n,0));
 
         int curDir=0;
@@ -11,13 +12,13 @@ public:
         while(num<=(n*n)){
             ans[r][c]=num++;
             
-            int nr=r+dir[curDir][0];
-            int nc=c+dir[curDir][1];
+            int nr=r+dir[curDir];
+            int nc=c+dir[curDir+1];
             if(nr<0 || nr>=n || nc<0 || nc>=n ||ans[nr][nc]!=0){
                 curDir=(curDir+1)%4;
             }
-            r=r+dir[curDir][0];
-            c=c+dir[curDir][1];
+            r=r+dir[curDir];
+            c=c+dir[curDir+1];
         }
         return ans;
     }
