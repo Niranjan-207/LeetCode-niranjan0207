5325c857a08ba4b3170dbd3cc1ba808e3501fac2
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-14 19:26:35 +0530

    Time: 0 ms (100%), Space: 72.8 MB (47.5%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        //map<int,int> mp;
         
         vector<int> fre(n+1,0);
-        for(vector<int> booking:bookings){
+        for(vector<int>& booking:bookings){
             fre[booking[0]-1]+=booking[2];
-            //if(booking[1]+1<n)
                 fre[booking[1]]-=booking[2];
         }
         for(int i=1;i<=n;i++){
@@ -17,11 +15,7 @@ public:
         for(int i=0;i<n;i++){
             ans[i]=fre[i];
         }
-        //int cnt=0;
-        // for(auto& [idx,delta]:mp){
-        //     cnt+=delta;
-        //     ans.push_back(cnt);
-        // }
+        
         return ans;
     }
 };
