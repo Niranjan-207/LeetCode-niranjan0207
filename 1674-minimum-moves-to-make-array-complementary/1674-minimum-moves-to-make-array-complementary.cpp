a45630fc0d87c2495e2bb0d0dc0ba97d4f26dec1
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-14 20:29:17 +0530

    Time: 12 ms (48.62%), Space: 93.5 MB (61.47%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums, int limit) {
-        int n = nums.size();
-        vector<int> dp(2 * limit + 2, 0);
-        for (int i = 0; i < n / 2; i++) {
-            int mini = min(nums[i], nums[n - 1 - i]);
-            int maxi = max(nums[i], nums[n - 1 - i]);
-            dp[2] += 2;
-            dp[mini + 1] -= 1;
-            dp[mini + maxi] -= 1;
-            dp[mini + maxi + 1] += 1;
-            dp[maxi + limit + 1] += 1;
-        }
-        int res = n;
-        int moves = 0;
-        for (int c = 2; c <= 2 * limit; c++) {
-            moves += dp[c];
-            res = min(res, moves);
+        int n=nums.size();
+        vector<int> dif(2*limit+2,0);
+        for(int i=0;i<(n/2);i++){
+            dif[2]+=2;
+            dif[2*limit+1]-=2;
+
+            int a=nums[i];
+            int b=nums[n-i-1];
+            int s=a+b;
+
+            int l=min(a,b)+1;
+            int r=max(a,b)+limit;
+
+            dif[l]+=(-1);
+            dif[r+1]-=(-1);
+            dif[s]+=(-1);
+            dif[s+1]-=(-1);
         }
 
-        return res;
+        for(int i=1;i<dif.size();i++){
+            dif[i]+=dif[i-1];
+        }
+        return *min_element(begin(dif)+2,end(dif)-1);
     }
 };
