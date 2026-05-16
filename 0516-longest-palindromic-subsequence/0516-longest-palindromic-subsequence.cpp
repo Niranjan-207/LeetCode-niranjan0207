c509c534ee4a0622d71299dbdc6e4369de6e0672
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-17 00:50:23 +0530

    Time: 88 ms (39.72%), Space: 76.3 MB (7.58%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -3,13 +3,12 @@ public:
     int f(int sidx,int revidx,string& s,string& rev,vector<vector<int>>& dp){
         if(sidx==s.length() || revidx==rev.length())    return 0;
         if(dp[sidx][revidx]!=-1)    return dp[sidx][revidx];
-        int match=0;
+        
         if(s[sidx]==rev[revidx]){
-            match= 1+f(sidx+1,revidx+1,s,rev,dp);
+            return dp[sidx][revidx]= 1+f(sidx+1,revidx+1,s,rev,dp);
         }
-        int notMatch=max(f(sidx+1,revidx,s,rev,dp),f(sidx,revidx+1,s,rev,dp));
-
-        return dp[sidx][revidx]=max(match,notMatch);
+        
+        return dp[sidx][revidx]=max(f(sidx+1,revidx,s,rev,dp),f(sidx,revidx+1,s,rev,dp));
     }
 
     int longestPalindromeSubseq(string s) {
