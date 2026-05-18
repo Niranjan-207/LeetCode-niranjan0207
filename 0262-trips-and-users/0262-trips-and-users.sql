3cc231885b8e453871242472faf96d8203a6ffab
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-19 01:08:42 +0530

    Time: 619 ms (20.25%), Space: 0B (100%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,8 @@
+SELECT request_at AS Day,
+    ROUND(SUM(CASE WHEN t.status!="completed" THEN 1 ELSE 0 END)*1.0/COUNT(*),2) AS 'Cancellation Rate'
+FROM Trips t 
+JOIN Users c ON t.client_id=c.users_id AND c.banned="No"
+JOIN Users d ON t.driver_id=d.users_id AND d.banned="No"
+WHERE t.request_at>='2013-10-01' AND t.request_at<='2013-10-03'
+GROUP BY request_at
+ORDER BY request_at DESC;
