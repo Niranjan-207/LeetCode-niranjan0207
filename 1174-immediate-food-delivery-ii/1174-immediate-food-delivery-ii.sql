a69b3bf5e64f58fa2a7ac46eeebbbbf52722bcd8
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-21 21:04:35 +0530

    Time: 639 ms (50.3%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,12 +1,7 @@
-WITH firstOrder AS(SELECT customer_id,
-        MIN(order_date) as order_date
+SELECT ROUND(AVG(CASE WHEN order_date=customer_pref_delivery_date THEN 1 ELSE 0 END)*100,2) AS immediate_percentage
 FROM Delivery
-GROUP BY customer_id),
-sameDayDelivery AS(
-    SELECT fo.customer_id
-    FROM firstOrder AS fo
-    JOIN Delivery d ON fo.customer_id=d.customer_id AND
-        fo.order_date=d.order_date
-        AND fo.order_date=d.customer_pref_delivery_date
-)
-SELECT ROUND((SELECT COUNT(*) FROM sameDayDelivery)/(SELECT COUNT(*) FROM firstOrder)*100,2) AS immediate_percentage;
+WHERE (customer_id,order_date) IN (
+    SELECT customer_id,MIN(order_date)
+    FROM DELIVERY 
+    GROUP BY customer_id
+);
