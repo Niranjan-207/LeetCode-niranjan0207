031d13cf65a591c04c4a8991578c9419cfcc3537
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-29 12:20:43 +0530

    Time: 1201 ms (11.14%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,14 @@
-WITH cte AS(
-    SELECT COUNT(*) as cnt FROM Users 
-)
-SELECT contest_id,ROUND((COUNT(*)/(SELECT cnt FROM cte))*100.00,2) as percentage
+-- WITH cte AS(
+--     SELECT COUNT(*) as cnt FROM Users 
+-- )
+-- SELECT contest_id,ROUND((COUNT(*)/(SELECT cnt FROM cte))*100.00,2) as percentage
+-- FROM Register
+-- GROUP BY contest_id
+-- ORDER BY percentage desc,contest_id;
+
+SELECT contest_id,ROUND(
+    (COUNT(*)/(SELECT COUNT(*) as cnt FROM Users ))*100.00,
+    2) as percentage
 FROM Register
 GROUP BY contest_id
 ORDER BY percentage desc,contest_id;
