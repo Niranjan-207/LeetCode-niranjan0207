10636ea4231e3e090f13a026567f37d636e696e2
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 23:26:58 +0530

    Time: 179 ms (5.67%), Space: 91.4 MB (8.47%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> se(begin(nums),end(nums));
+
+        int ans=0;
+        for(int num:se){
+            if(!se.count(num-1)){
+                int cur=num+1;
+
+                int len=1;
+                while(se.count(cur++))  len++;
+
+                ans=max(ans,len);
+            }
+        }
+        return ans;
+    }
+};
